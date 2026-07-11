414bc92a6303f9985f37f26b395ea0c9e246ec41
At87668 <author87668@hotmail.com>
2026-07-11 17:26:52 +0800

    refactor: replace wildcard imports with explicit imports (MiningState.java)

--- src/main/java/link/star_dust/MinerTrack/core/detection/MiningState.java
+++ src/main/java/link/star_dust/MinerTrack/core/detection/MiningState.java
@@ -4,7 +4,14 @@ import link.star_dust.MinerTrack.common.CommonLocation;
 import link.star_dust.MinerTrack.common.DetectionBridge;
 import link.star_dust.MinerTrack.core.CoreLogger;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
