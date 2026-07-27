18ed988b06548498286ce8ef642bf4d8d6e72b29
At87668 <author87668@hotmail.com>
2026-07-27 19:34:54 +0800

    chore: add copyright header

--- src/main/java/link/star_dust/MinerTrack/common/ViolationManagerBridge.java
+++ src/main/java/link/star_dust/MinerTrack/common/ViolationManagerBridge.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of MinerTrack, licensed under the GNU General Public License v3.0.
+ *
+ *  Copyright (c) At87668 (Author87668) <https://github.com/At87668>
+ *  Copyright (c) contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package link.star_dust.MinerTrack.common;
 
 import java.util.UUID;
